0a7fc1d963ca8d09f253103908c53cede865645b
ChristianHarborow <christianharborow@hotmail.co.uk>
2024-07-01 12:21:38 +0100

    adds Page classes

---
+++ src/test/java/com/sparta/lib/pages/ProductsPage.java
@@ -0,0 +1,9 @@
+package com.sparta.lib.pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class ProductsPage extends WebPage{
+    public ProductsPage(WebDriver webDriver) {
+        super(webDriver);
+    }
+}
