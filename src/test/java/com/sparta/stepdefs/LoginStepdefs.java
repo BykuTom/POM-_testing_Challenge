1532e893025cb8bf379e5884fda6924a4270f6f0
RazorWinds <logan.marshall.dev@gmail.com>
2024-07-01 14:28:58 +0100

    LoginDefs

--- src/test/java/com/sparta/stepdefs/LoginStepdefs.java
+++ src/test/java/com/sparta/stepdefs/LoginStepdefs.java
@@ -1,17 +1,31 @@
 package com.sparta.stepdefs;
 
+import com.sparta.lib.pages.LoginPage;
+import io.cucumber.datatable.DataTable;
 import io.cucumber.java.After;
 import io.cucumber.java.AfterAll;
 import io.cucumber.java.Before;
 import io.cucumber.java.BeforeAll;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-public class LoginStepdefs implements StepDefable{
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class LoginStepdefs implements StepDefable {
     static ChromeDriverService service;
     WebDriver webDriver;
 
@@ -25,17 +39,89 @@ public class LoginStepdefs implements StepDefable{
     }
 
     @Before
-    public void setUp(){
+    public void setUp() {
         webDriver = new RemoteWebDriver(service.getUrl(), getChromeOptions());
     }
 
     @After
-    public void afterEach(){
+    public void afterEach() {
         webDriver.quit();
     }
 
     @AfterAll
-    public static void afterAll(){
+    public static void afterAll() {
         service.stop();
+    }
+
+
+    @Given("the user is on the login page")
+    public void theUserIsOnTheLoginPage() {
+        webDriver.get("https://www.saucedemo.com/v1/index.html");
+    }
+
+
+    @When("the user enters a valid username and password")
+    public void
+    theUserEntersAValidUsernameAndPassword
+            (DataTable dataTable) {
+        List<Map<String, String>> data = dataTable.asMaps(String.class, String.class);
+        for
+        (Map<String, String> row : data) {
+            System.out.println(
+                    "Username: "
+                            + row.get(
+                            "username"
+                    ));
+            System.out.println(
+                    "Password: "
+                            + row.get(
+                            "password"
+                    ));
+            LoginPage
+                    page =
+                    new
+                            LoginPage
+                            (webDriver);
+            page.enterUsername(row.get(
+                    "username"
+            ));
+            page.enterPassword(row.get(
+                    "password"
+            ));
+        }
+    }
+
+    @And("clicks the login button")
+    public void clicksTheLoginButton() {
+        LoginPage page = new LoginPage(webDriver);
+
+    }
+
+    @Then("the user should be redirected to the Product page")
+    public void theUserShouldBeRedirectedToTheProductPage() {
+        assertEquals("https://www.saucedemo.com/v1/inventory.html", webDriver.getCurrentUrl());
+    }
+
+
+    @When("the user enters an invalid username or password")
+    public void theUserEntersAnInvalidUsernameOrPassword(DataTable data) {
+        Map<String, String> map = data.asMap();
+        LoginPage page = new LoginPage(webDriver);
+        page.enterPassword(map.get("password"));
+        page.enterUsername(map.get("username"));
+    }
+
+    @Then("the user should see an error message {string}")
+    public void theUserShouldSeeAnErrorMessage(String arg0) {
+        LoginPage page = new LoginPage(webDriver);
+        assertEquals(webDriver.findElement(By.cssSelector("*[data-test=\"error\"]")).getText(), "Epic sadface: Username and password do not match any user in this service");
+
+    }
+
+
+    @When("the user leaves the username field empty and enters a valid password")
+    public void theUserLeavesTheUsernameFieldEmptyAndEntersAValidPassword() {
+        LoginPage page = new LoginPage(webDriver);
+        page.enterPassword("secret_sauce");
     }
 }
--- src/test/resources/features/login.feature
+++ src/test/resources/features/login.feature
@@ -4,23 +4,28 @@ Feature: User Login
   I want to log in to the application
   So that I can access my personalized dashboard and use the application’s features
 
-Scenario: Successful login with valid credentials
-Given the user is on the Login page
-When the user enters a valid username and password
-And clicks the login button
-Then the user should be redirected to the Product page
-
+  Scenario: Successful login with valid credentials
+    Given the user is on the login page
+    When the user enters a valid username and password
+      | username       | password     |
+      | standard_user  | secret_sauce |
+    And clicks the login button
+    Then the user should be redirected to the Product page
 
   Scenario: Unsuccessful login with invalid credentials
-    Given the user is on the Login page
+    Given the user is on the login page
     When the user enters an invalid username or password
+      | username      | password     |
+      | invalid       | secret_sauce |
+      | standard_user | invalid      |
+      | invalid       | invalid      |
     And clicks the login button
     Then the user should see an error message "Epic sadface: Username and password do not match any user in this service"
-    And the user should remain on the Login page
 
   Scenario: Unsuccessful login with only password entered
-    Given the user is on the Login page
+    Given the user is on the login page
     When the user leaves the username field empty and enters a valid password
+      | password     |
+      | secret_sauce |
     And clicks the login button
     Then the user should see an error message "Epic sadface: Username is required"
-    And the user should remain on the Login page
