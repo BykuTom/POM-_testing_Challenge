6e25b0f2e6614e8277437d41284a72994e04cfda
christian harborow <christianharborow@hotmail.co.uk>
2024-07-02 11:24:21 +0100

    implements feature step defs

--- src/test/java/com/sparta/stepdefs/ProductsStepdefs.java
+++ src/test/java/com/sparta/stepdefs/ProductsStepdefs.java
@@ -1,9 +1,16 @@
 package com.sparta.stepdefs;
 
+import com.sparta.lib.pages.CheckoutPage;
+import com.sparta.lib.pages.LoginPage;
+import com.sparta.lib.pages.ProductsPage;
 import io.cucumber.java.After;
 import io.cucumber.java.AfterAll;
 import io.cucumber.java.Before;
 import io.cucumber.java.BeforeAll;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.remote.RemoteWebDriver;
@@ -11,7 +18,14 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 import java.io.File;
 import java.io.IOException;
 
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
+
 public class ProductsStepdefs implements StepDefable{
+    private String[] productNames;
+    private LoginPage loginPage;
+    private ProductsPage productsPage;
+    private CheckoutPage checkoutPage;
 
     static ChromeDriverService service;
     WebDriver webDriver;
@@ -38,5 +52,47 @@ public class ProductsStepdefs implements StepDefable{
     @AfterAll
     public static void afterAll() {
         service.stop();
+    }
+
+    @Given("I am logged in as a {string}")
+    public void iAmLoggedInAsA(String userType) {
+        webDriver.get("https://www.saucedemo.com/v1/");
+        loginPage = new LoginPage(webDriver);
+        loginPage.enterUsername(userType);
+        loginPage.enterPassword("secret_sauce");
+        loginPage.clickSignIn();
+        productsPage = new ProductsPage(webDriver);
+    }
+
+    @When("I click add to cart button for the products {string}")
+    public void iClickAddToCartButtonForTheProducts(String productList) {
+
+        productNames = productList.split(",");
+        for (String name : productNames) {
+            productsPage.addToCart(name);
+        }
+    }
+
+    @Then("the buttons text switches to REMOVE")
+    public void theButtonsTextSwitchesToREMOVE() {
+        for (String name : productNames) {
+            String buttonText = productsPage.getProductButtonText(name);
+            assertThat(buttonText, is("REMOVE"));
+        }
+    }
+
+    @And("the cart icon count becomes {int}")
+    public void theCartIconCountBecomes(int count) {
+        int cartCount = productsPage.getCartIconCount();
+        assertThat(cartCount, is(count));
+    }
+
+    @And("the products appear on the cart page")
+    public void theProductsAppearOnTheCartPage() {
+        webDriver.get("https://www.saucedemo.com/v1/cart.html");
+        checkoutPage = new CheckoutPage(webDriver);
+        for (String name : productNames) {
+            assertThat(checkoutPage.productIsInCart(name), is(true));
+        }
     }
 }
--- src/test/resources/features/products.feature
+++ src/test/resources/features/products.feature
@@ -4,11 +4,11 @@ As a user I need to be able to add products to the cart so I can save them for l
 
 Scenario: Adding products to cart as a standard user
 
-Given I am logged in as a [USER_TYPE]
-When I click "add to cart" button for the products [PRODUCT_NAME_LIST]
-Then the buttons text switches to "Remove"
-And the cart icon count becomes [NUMBER_OF_PRODUCTS]
-And the products [PRODUCT_NAME_LIST] appear on the cart page
+Given I am logged in as a "standard_user"
+When I click add to cart button for the products "Sauce Labs Backpack"
+Then the buttons text switches to REMOVE
+And the cart icon count becomes 1
+And the products appear on the cart page
 
 Scenario: Removing products from the cart
 
